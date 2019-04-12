b8570be12638b01b9fbd7a2aeae8f71d21232410
pananon <pandeyjss01@gmail.com>
2019-04-13 01:12:09 +0530

    Basic Static webpages with template

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const express = require("express")
 
+const expressEdge = require("express-edge")
+
+const path = require("path")
+
+const mongoose = require("mongoose")
+
 const app = new express()
 
+mongoose.connect('mongodb://localhost/breakinpropaganda')
+
+app.use(express.static('public'))
+
+app.use(expressEdge)
+
+app.set('views', `${__dirname}/views`);
+
+app.get('/',(req,res)=>{
+	res.render('index')
+
+})
+app.get('/about',(req,res)=>{
+	res.render('about')
+
+})
+app.get('/post',(req,res)=>{
+	res.render('post')
+
+})
+app.get('/contact',(req,res)=>{
+	res.render('contact')
+
+})
 
 app.listen(4000,()=>{
-	console.log("Server Successfully created")
+	console.log("Server Successfully created and running")
 })
